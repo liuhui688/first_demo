8db579867541792030f0dc5496da44e2aecaca3a
liuhui688 <1683908585@qq.com>
2024-01-25 23:15:13 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    port:7070,
+    port:7071,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
